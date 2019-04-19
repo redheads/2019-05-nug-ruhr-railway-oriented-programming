7c151ec337bf92f5b805aeea2ff2426a1228e5e4
Patrick Drechsler <patrick@pdrechsler.de>
2019-04-20 01:14:26 +0200

    code: setup project with vs2017

---
+++ code/RopDemos/RopDemos.Slides/SlideTests.cs
@@ -0,0 +1,14 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace RopDemos.Slides
+{
+    public class SlideTests
+    {
+        [Fact]
+        public void SmokeTest()
+        {
+            true.Should().BeTrue();
+        }
+    }
+}
