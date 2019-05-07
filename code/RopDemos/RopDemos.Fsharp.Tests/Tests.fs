b50cc886fc1a34b16278abd67e2a3741357195f4
Patrick Drechsler <patrick.drechsler@redheads.de>
2019-05-08 00:48:48 +0200

    code: remove broken fsharp

--- code/RopDemos/RopDemos.Fsharp.Tests/Tests.fs
+++ code/RopDemos/RopDemos.Fsharp.Tests/Tests.fs
@@ -4,21 +4,6 @@ open System
 open Xunit
 open FsUnit.Xunit
 
-type CustomerName = private CustomerName of string
-
-let create s = 
-    match String.IsNullOrWhiteSpace(s) with
-    | true -> Error [ "String must not be empty" ]
-    | false -> Ok(CustomerName s)
-
-let get (CustomerName cn) = cn
-
-type Customer = {
-    CustomerName : CustomerName
-}
-
-
-
 [<Fact>]
 let ``My test`` () =
     Assert.True(true)
